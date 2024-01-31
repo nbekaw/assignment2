ae2c292d7fba6debe9539aadf67b2b1a18136820
nbekaw <154734017+nbekaw@users.noreply.github.com>
2024-02-01 05:04:41 +1100

    Person.java

--- Assignmnt2/Person.java
+++ Assignmnt2/Person.java
@@ -1,5 +1,5 @@
 public abstract class Person implements Payable,Comparable<Person> {
-    private int id;
+    private int id; //creating all variables
     private static int CounterId = 0;
     private String name;
     private String surname;
@@ -7,11 +7,13 @@ public abstract class Person implements Payable,Comparable<Person> {
     }
     public Person(String name,String surname) {
         CounterId++;
-        id = CounterId;
+        id = CounterId; //add +1 to id for every person
         setName(name);
         setSurname(surname);
     }
 
+
+    // just getters and setters
     public int GetID(){
         return id;
     }
@@ -26,19 +28,18 @@ public abstract class Person implements Payable,Comparable<Person> {
     public String getName() {
         return name;
     }
-
-    public String getSurname() {
-        return surname;
-    }
-
+    @Override
     public int compareTo(Person person) {
         return Double.compare(this.getPaymentAmount(), person.getPaymentAmount());
     }
 
+    // Abstract method to be implemented by concrete subclasses
     public abstract String getPosition();
+
+    // Custom string representation of the Person object
     @Override
     public String toString() {
-        return  getPosition() + " " +
+        return getPosition() + " " +
                 "id = " + GetID() + ", name = " + name + ", surname = " + surname;
     }
 }
