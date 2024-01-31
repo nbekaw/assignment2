0adb038969ad4dfc1413f3385d2d88a773154fd6
nbekaw <154734017+nbekaw@users.noreply.github.com>
2024-02-01 04:51:37 +1100

    Student.java

--- Assignmnt2/Student.java
+++ Assignmnt2/Student.java
@@ -1,14 +1,17 @@
-public class Student extends Person{
-    private  double gpa;
+import java.util.Comparator;
 
-    public Student(){
+public class Student extends Person {
+    private double gpa;
+
+    // calls superclass person
+    public Student() {
         super();
     }
-    public Student(String name,String surname,double gpa){
-        super(name,surname);
+
+    public Student(String name, String surname, double gpa) {
+        super(name, surname);
         setGpa(gpa);
     }
-
 
     public void setGpa(double gpa) {
         this.gpa = gpa;
@@ -23,21 +26,17 @@ public class Student extends Person{
         return "Student";
     }
 
+    // checks gpa and returns scholarship. Low gpa = no money
     @Override
-    public double getPaymentAmount(){
-        if(this.gpa>2.67){
-            return 36660.00;
-        }
-        else{
-            return 0;
-        }
+    public double getPaymentAmount() {
+        return (this.gpa > 2.67) ? 36660.00 : 0;
     }
 
+    // Overridden toString() method to provide a custom string representation of the Student object.
     @Override
     public String toString() {
-        return super.toString()+
-                "\ngpa = " + gpa+
-                "\nsalary = " + getPaymentAmount()+" tg\n";
-
+        return super.toString() +
+                "\ngpa = " + gpa +
+                "\nsalary = " + getPaymentAmount() + " tg\n";
     }
 }
